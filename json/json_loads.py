49cef5398d2c628ef9f1b3590b41a3f4dac3188f
ray <shanei5107@gmail.com>
2022-06-13 15:49:42 +0800

    fix: json

--- json/json_loads.py
+++ json/json_loads.py
@@ -1,7 +1,4 @@
 # -*- coding: utf-8 -*-
-
-import json
-
 '''
 方法	         作用
 json.dumps()	将 Python 对象转换成 JSON 字符串。
@@ -9,6 +6,8 @@ json.loads()	将 JSON 字符串转换成 Python 对象。
 json.dump()  	将 Python 中的对象转化成 JSON 字符串储存到文件中。
 json.load() 	将文件中的 JSON 字符串转化成 Python 对象提取出来。
 '''
+
+import json
 
 # 转为python对象
 website_info = '{"name":"测试","pv":"50万"}'
@@ -27,9 +26,3 @@ with open('web.json', 'r') as f:
 # 将对象转json字符串
 items = json.dumps(dict_info, ensure_ascii=False)
 print("转换之后的数据类型为:", type(items))
-
-方法	作用
-json.dumps()	将 Python 对象转换成 JSON 字符串。
-json.loads()	将 JSON 字符串转换成 Python 对象。
-json.dump()	将 Python 中的对象转化成 JSON 字符串储存到文件中。
-json.load()	将文件中的 JSON 字符串转化成 Python 对象提取出来。
